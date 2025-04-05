4966f1c7235bf7ab52d772f161bd60ab2e5594a7
Adam Sarenski <asarenski@gmail.com>
2025-04-05 14:13:14 -0400

    updated module structure with: main, model

---
+++ model/src/main/java/org/asarenski/model/Coordinates.java
@@ -0,0 +1,40 @@
+package org.asarenski.model;
+
+/**
+ * Represents a position on the Craps game board.
+ */
+public class Coordinates {
+    private final int x;
+    private final int y;
+
+    public Coordinates(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Coordinates that = (Coordinates) obj;
+        return x == that.x && y == that.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return 31 * x + y;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("(%d, %d)", x, y);
+    }
+} 
